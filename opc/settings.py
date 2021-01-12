594358ed00ce34341e0cad4f7557a1cf3c7a7b70
SebastianUrbano <42156534+SebastianUrbano@users.noreply.github.com>
2021-01-11 23:42:32 -0500

    Fix database host
    Si la dejabamos con esa IP todas las instancias de opx quedaban apuntando a ella

--- opc/settings.py
+++ opc/settings.py
@@ -95,7 +95,7 @@ DATABASES = {
         'NAME': 'opx_dev',
         'USER': 'opxuser',
         'PASSWORD': 'opx_dev_pass',
-        'HOST': '104.155.149.1',
+        'HOST': 'localhost',
         'PORT': '5432',
     }
 }
